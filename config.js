0765d52a2a6e825f5a9ab050cc89b0debc5c51e8
Loc Nguyen <duclocdk1999@gmail.com>
2023-03-08 22:42:27 +0700

    Update code for Truc

--- config.js
+++ config.js
@@ -1,19 +1,18 @@
 const CONFIG = {
     introTitle: 'Em iu à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Em là ngọn gió đời tôi
+    Bao yêu thương xin gửi vào trong gió
+    Gió ơi`,
     btnIntro: 'hihi',
-    title: 'Phải chăng bé Hiền đã yêu Boy ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng bé Hiền đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Bé Hiền mãi iu anh <3',
-    btnNo: 'Không, ai rảnh iu đương gì mấy người -_-',
-    question:'Trên thế giới hơn 7 tỉ người mà sao bé Hiền lại yêu anh vậy <3',
+    title: 'Phải chăng bé Trúc đã yêu anh Lộc ngay từ cái nhìn đầu tiên?',
+    desc: 'Phải chăng bé Trúc đã say ngay từ lúc thấy nụ cười ấy?',
+    btnYes: 'Mãi yêu anh Lộc <3',
+    btnNo: 'Không, ai mà yêu anh cơ chứ -_-',
+    question:'Trên thế giới hơn 7 tỉ người mà sao bé Trúc lại yêu anh vậy <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Lộc biết mà 🥰. Yêu Ân nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h mình chịch nhé công chúa.',
-    btnAccept: 'Okiiiii anh iu lun <3',
+    reply: 'Vì anh Lộc đẹp trai <3 <3 <3 <3 <3',
+    mess: 'Lộc biết mà 🥰. Yêu Trúc nhiều nhiều 😘😘',
+    messDesc: 'Chúc bé Trúc một ngày 8/3 ngọt ngào tràn đầy ý nghĩa <3',
+    btnAccept: 'Okie anh iu lun <3',
     messLink: 'https://www.messenger.com/t/100004364447115'
 }
